032dbfba3726d1a53878785331f0c8172dd3434f
Pusakaofjava Indonesia Corp <kreatifi@kreatifindonesia.com>
2024-11-19 10:53:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import subprocess
 
 def main():
     print("Generating dataset...")
-    subprocess.run(["python", "data/dataset_generator.py"])
+    subprocess.run(["python3", "data/dataset_generator.py"])
 
     print("Training model...")
-    subprocess.run(["python", "scripts/train.py"])
+    subprocess.run(["python3", "scripts/train.py"])
 
     print("Making predictions...")
-    subprocess.run(["python", "scripts/predict.py"])
+    subprocess.run(["python3", "scripts/predict.py"])
 
 if __name__ == "__main__":
     main()
